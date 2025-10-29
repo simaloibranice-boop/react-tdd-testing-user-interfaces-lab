5d680f53ba29a1fdaadcc26e854db01b55a11bd1
Branice Simaloi <simaloibranice@gmail.com>
2025-10-29 09:30:23 +0300

    completed react-tdd-testing-user-interfaces-lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React from "react";
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+  <div>
+    <h1>Hi, I'm Branice</h1>
+    <img
+      src='https://example.com/branice.jpg'
+      alt='Branice smiling in front of a car'
+       />
+       <h2>About Me.</h2>
+       <p>
+        I'm Branice, a passionate web develope who loves building digital
+        experiences and automotive tech solutions that connect people.
+       </p>
+       <h3>Connect with me.</h3>
+       <ul>
+        <li><a href="htttps://github.com/simaloibranice-boop" target="_blank" rel="nonreferrer">Github</a></li>
+        <li><a href="https://linkedin.com/in/branice" target="_blank" rel="noreferrer">Linkedin</a></li>
+       </ul>
+   </div>
+    );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,4 +3,49 @@ import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+test("displays a top-level heading with the text 'Hi, I\'m ____'", () => {
+    render(<App />);
+    const topLevelHeading = screen.getByRole('heading', {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1,
+    });
+
+    expect(topLevelHeading).toBeInTheDocument();
+});
+
+test('displays an image of myself with descriptive alt text', () => {
+    render(<App />);
+
+    const image = screen.getByAltText(/branice/i);
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src');
+});
+
+test("displays a second-level heading with the text 'About me' and a short biography", () => {
+    render(<App />);
+
+    const aboutHeading = screen.getByRole('heading', {
+        name: /about me/i,
+        level: 2,
+    });
+
+    expect(aboutHeading).toBeInTheDocument();
+
+    const bioParagraph = screen.getByText(/developer|designer|creator|passionate/i);
+    expect(bioParagraph).toBeInTheDocument();
+});
+
+test('display links to my Github and Linkedin profiles', () =>{
+    render(<App />);
+
+    const githubLink = screen.getByRole('link', {name: /github/i});
+    const linkedinLink = screen.getByRole('link', {name: /linkedin/i});
+
+    expect(githubLink).toBeInTheDocument();
+    expect(linkedinLink).toBeInTheDocument();
+
+    expect(githubLink).toHaveAttribute('href', expect.stringContaining('github.com'));
+    expect(linkedinLink).toHaveAttribute('href',expect.stringContaining('linkedin.com'));
+});
